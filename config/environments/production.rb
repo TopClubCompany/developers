a878d5e68792fde0907cbe28209d80971050b126
Alex Opak <opak.alexandr@gmail.com>
2012-11-14 13:30:32 +0200

    fix production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,13 @@ Topclub::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.default_url_options = {:host => 'top-club'}
+  config.action_mailer.delivery_method = :sendmail
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
