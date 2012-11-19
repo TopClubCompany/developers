0e6001c10a74446fa52c5d8bec12142b3600b1d0
Alex Opak <opak.alexandr@gmail.com>
2012-11-19 08:58:27 +0200

    fix

--- app/models/locator.rb
+++ app/models/locator.rb
@@ -1,13 +1,27 @@
+# -*- encoding : utf-8 -*-'
 class Locator
   # attr_accessible :title, :body
   def self.find(*args)
 
   end
 
+  def self.model_name
+    Locator
+  end
+
+  def self.plural
+    "locators"
+  end
+
+  def self.human(count)
+    "Переводы"#I18n.t('admin.locator')
+  end
+
   def title
     ''
   end
 end
+
 # == Schema Information
 #
 # Table name: locators
