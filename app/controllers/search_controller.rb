b08605427f726cb01dbc1e669283e5ece265e5a8
Yaroslav Kotsur <yarikkotsur@topclub.kiev.ua>
2012-11-30 15:12:03 +0200

    default view changed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,22 @@
 class SearchController < ApplicationController
 
+  def index
+    @categories = Category.order :created_at
+    @reviews    = Review.order :created_at
+    @selections = Selection.order :created_at
+    @recomended_places = Place.first 7
+
+    lang = request.env['REQUEST_URI'].to_s.split('/')[0]
+    p lang
+    if lang.nil?
+      @lang = 'en'
+    elsif lang =~ /(ru|ua|en)/
+      @lang = lang
+    end
+    #styx_initialize_with point: @current_point
+    render :template => 'search/index'
+  end
+
   def search
 
   end
