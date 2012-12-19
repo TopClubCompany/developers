1028a77345348f306e986ceca2456a480f804a18
Alex Opak <opak.alexandr@gmail.com>
2012-12-19 08:54:17 +0200

    fix

--- app/metal/autocomplete_app.rb
+++ app/metal/autocomplete_app.rb
@@ -5,16 +5,17 @@ class AutocompleteApp
     session = env['rack.session']
 
     params['q'] = '' unless params['q']
+    options = {}
 
     #options[:order] = "overall_mark"
     #options[:sort_mode] = "desc"
     options[:title] = params['q']
+    options[:per_page] = params['per_page'] if params['per_page'].present?
     options[:city] = (session['city'] || 'kyiv')
 
     result = Place.search(options)
-    result.map{|e| Place.for_autocomplite(e)}
 
-    [200, {"Content-Type" => "application/json"}, result.map{|e| Place.for_autocomplite(e)}]
+    [200, {"Content-Type" => "application/json"}, result.map{|e| Place.for_autocomplite(e)}.to_json]
 
   end
 
