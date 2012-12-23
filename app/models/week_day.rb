b2e5c40e70a7e7ead898e37bf9b1b72d80fe88d4
sktocha <sktocha@Inspiron.(none)>
2012-12-23 16:41:37 +0200

    merge week_days && discount_day_schedule

--- app/models/week_day.rb
+++ app/models/week_day.rb
@@ -1,29 +1,44 @@
 class WeekDay < ActiveRecord::Base
-  attr_accessible :title, :start_at, :end_at, :start_break_at, :end_break_at, :place_id
+  attr_accessible :title, :start_at, :end_at, :start_break_at, :end_break_at, :place_id,
+                  :day_discounts_attributes, :day_type_id
 
   belongs_to :place
 
-  translates :title
 
   include Utils::Models::Base
-  include Utils::Models::Translations
   include Utils::Models::AdminAdds
+
+  has_many :day_discounts, :dependent => :destroy
+
+  accepts_nested_attributes_for :day_discounts,
+                                :allow_destroy => true, :reject_if => :all_blank
+
+  enumerated_attribute :day_type, :id_attribute => :day_type_id, :class => ::DayType
+
+  def self.create_for_new_place
+    ::DayType.all.map do |day|
+      self.new({day_type_id: day.id})
+    end
+  end
+
 end
 # == Schema Information
 #
 # Table name: week_days
 #
 #  id             :integer          not null, primary key
-#  start_at       :float
-#  end_at         :float
-#  start_break_at :float
-#  end_break_at   :float
+#  start_at       :time
+#  end_at         :time
+#  start_break_at :time
+#  end_break_at   :time
+#  day_type_id    :integer          not null
 #  place_id       :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #
 # Indexes
 #
-#  index_week_days_on_place_id  (place_id)
+#  index_week_days_on_day_type_id  (day_type_id)
+#  index_week_days_on_place_id     (place_id)
 #
 
