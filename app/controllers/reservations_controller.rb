1b8a0927ed953fb22bd7a078700dce27f4b2f3e8
sktocha <sktocha@Inspiron.(none)>
2012-12-25 15:23:06 +0200

    reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,36 @@
 class ReservationsController < ApplicationController
 
   def new_reservation
+    date = params[:date].match /(?<day>\d+)-(?<month>\d+)-(?<year>\d+)/
+    time = params[:time].match /h=(?<hour>\d+)&m=(?<minutes>\d+)/
 
+    @persons = params[:amount_of_person]
+    @place   = Place.find_by_id(params[:place_id])
+    @date    = DateTime.new(date[:year].to_i, date[:month].to_i, date[:day].to_i,
+                            time[:hour].to_i, time[:minutes].to_i)
+    @reservation = Reservation.new(first_name: current_user.try(:first_name), last_name: current_user.try(:last_name),
+                                  phone: current_user.try(:phone), user_id: current_user.try(:id), place_id: @place.id,
+                                  email: current_user.try(:email))
   end
 
   def reservation_confirmed
+    @reservation = Reservation.find_by_id(params[:reservation_id])
+    @place = @reservation.try(:place)
+    @date  = DateTime@reservation.try(:time)
+    #raise @reservation.inspect
+    redirect_to root_path, flash: { error: 'no such reservation' } unless @reservation && @place
+  end
 
+
+  def complete_reservation
+    reservation = Reservation.new(params[:reservation])
+
+    if reservation.save
+      AccountMailer.new_reservation(User.first.email, reservation.id).deliver
+      redirect_to reservation_confirmed_path(reservation.id)
+    else
+      redirect_to new_reservation_path(@reservation), flash: { error: @reservation.errors.full_messages.join(', ') }
+    end
   end
 
 end
