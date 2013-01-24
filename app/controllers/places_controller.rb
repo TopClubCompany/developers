effdc43f0fb0f57a8496cdf24e01f1dc28191478
blobblob <snob.blobblob@gmail.com>
2013-01-24 02:56:14 +0200

    some additions for place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,18 @@
 #coding: utf-8
 class PlacesController < ApplicationController
   def show
+    if params[:reserve_date].present? and params[:reserve_date].length > 1
+      @date = params[:reserve_date]
+    else
+      @date = Date.today.strftime('%d/%m/%Y')
+    end
+    if params[:reserve_time].present? and params[:reserve_time].length > 1
+      time = Time.parse(params[:reserve_time])
+    else
+      time = Time.now
+    end
+    minutes = ['00', '30'][time.min / 30]
+    @time = {:h => time.hour.to_s, :m => minutes}
     @place = Place.find params[:id]
     @location = (Place.find params[:id]).lat_lng
     @special_offers =  @place.day_discounts.where(is_discount: false)
