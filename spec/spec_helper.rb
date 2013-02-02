496e392cd10b4929be82777dc855d05966be4c4c
Alex Opak <opak.alexandr@gmail.com>
2013-02-02 19:15:51 +0200

    fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "active_support"
 require 'spork'
 require 'rspec/rails'
+require 'rspec/autorun'
 require 'capybara/rails'
 require 'simplecov'
 
