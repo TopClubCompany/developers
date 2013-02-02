9c24eee6b0ba8ebab360780cbec0c9d2b035230d
Alex Opak <opak.alexandr@gmail.com>
2013-02-02 17:15:43 +0200

    fix

--- lib/place_utils/place_time.rb
+++ lib/place_utils/place_time.rb
@@ -6,20 +6,16 @@ module PlaceUtils
         reverse_time = time.strftime("%H.%M").to_f
         end_time = (Time.parse(end_time.to_s.sub(".",":")) - 60.minutes).strftime("%H.%M").to_f
         range_time = (start_time...end_time)
+        if reverse_time == 16.00 && time_now <= 15.00 && end_time <= 23.00
+          {:time => (time + i.minutes).strftime("%H:%M").to_sym,
+           :available => (i > 0 && is_available?(time+i.minutes, range_time))}
+        else
           {:time => (time + i.minutes).strftime("%H:%M").to_sym, :available => is_available?(time+i.minutes, range_time)}
+        end
       end
 
       def is_available?(time, range_time)
         range_time.cover? time.strftime("%H.%M").to_f
-      end
-
-      def wday wday
-        case wday
-          when 0
-            7
-          else
-            wday
-        end
       end
     end
   end
