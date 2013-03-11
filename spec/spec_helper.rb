16c15aea622d527dad0323cb41ce9fc1773e61b4
Alex Opak <opak.alexandr@gmail.com>
2013-03-11 23:43:42 +0200

    fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,48 +5,48 @@ require 'spork'
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
-require 'simplecov'
+require 'simplecov' 
 
-Spork.prefork do
-  unless ENV['DRB']
-    SimpleCov.start 'rails'
-  end
-  ENV['RAILS_ENV'] ||= 'test'
-  #require File.expand_path('../../config/environment', __FILE__)
-  Dir[Rails.root.join('spec/test/*.rb')].each{|f| require f}
-
-  RSpec.configure do |config|
-    config.mock_with :rspec
-    config.fixture_path = "#{Rails.root}/spec/fixtures"
-    #config.use_transactional_fixtures = true
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
-    config.before(:all) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:all) do
-      DatabaseCleaner.clean
-    end
-    ActiveSupport::Dependencies.clear
-  end
-
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.add_mock(:facebook, {
-      info:  { name: "Facebook Smith", nickname: 'joesmith', email: 'facebook@haha.com', uid: 'fb_id' },
-      extra: {raw_info: { name: "Facebook Smith" } }})
-  OmniAuth.config.add_mock(:google, {
-      info:  { name: "Google Smith", nickname: 'joesmith', email: 'google@haha.com', uid: 'google_id' },
-      extra: {raw_info: { name: "Google Smith" } }})
-  OmniAuth.config.add_mock(:vkontakte, {
-      info:  { name: "Vkontakte Smith", nickname: 'joesmith', uid: 'vk_id' },
-      extra: {raw_info: { name: "Joe Smith" } }})
-  OmniAuth.config.add_mock(:twitter, {
-      info:  { name: "Twitter Smith", nickname: 'joesmith', uid: 'tw_id' },
-      extra: {raw_info: { name: "Twitter Smith" } }})
-end
+#Spork.prefork do
+#  unless ENV['DRB']
+#    SimpleCov.start 'rails'
+#  end
+#  ENV['RAILS_ENV'] ||= 'test'
+#  #require File.expand_path('../../config/environment', __FILE__)
+#  Dir[Rails.root.join('spec/test/*.rb')].each{|f| require f}
+#
+#  RSpec.configure do |config|
+#    config.mock_with :rspec
+#    config.fixture_path = "#{Rails.root}/spec/fixtures"
+#    #config.use_transactional_fixtures = true
+#    config.before(:suite) do
+#      DatabaseCleaner.strategy = :transaction
+#      DatabaseCleaner.clean_with(:truncation)
+#    end
+#    config.before(:all) do
+#      DatabaseCleaner.start
+#    end
+#
+#    config.after(:all) do
+#      DatabaseCleaner.clean
+#    end
+#    ActiveSupport::Dependencies.clear
+#  end
+#
+#  OmniAuth.config.test_mode = true
+#  OmniAuth.config.add_mock(:facebook, {
+#      info:  { name: "Facebook Smith", nickname: 'joesmith', email: 'facebook@haha.com', uid: 'fb_id' },
+#      extra: {raw_info: { name: "Facebook Smith" } }})
+#  OmniAuth.config.add_mock(:google, {
+#      info:  { name: "Google Smith", nickname: 'joesmith', email: 'google@haha.com', uid: 'google_id' },
+#      extra: {raw_info: { name: "Google Smith" } }})
+#  OmniAuth.config.add_mock(:vkontakte, {
+#      info:  { name: "Vkontakte Smith", nickname: 'joesmith', uid: 'vk_id' },
+#      extra: {raw_info: { name: "Joe Smith" } }})
+#  OmniAuth.config.add_mock(:twitter, {
+#      info:  { name: "Twitter Smith", nickname: 'joesmith', uid: 'tw_id' },
+#      extra: {raw_info: { name: "Twitter Smith" } }})
+#end
 #
 #
 #
