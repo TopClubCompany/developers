4924a9b251d32f4e6cb753401ccd4e4a1ed1ca74
Alex Opak <opak.alexandr@gmail.com>
2013-03-13 10:58:22 +0200

    fix

--- lib/utils/i18none/alphavit.rb
+++ lib/utils/i18none/alphavit.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
+require "unicode_utils/downcase"
+
 module Utils
   module I18none
     module Alphavit
@@ -10,13 +12,15 @@ module Utils
       end
 
       def self.to_en(text)
-        ru_mapping = {"а" => "a", "б" => "b", "г" => "g", "д" => "d", "е" => "e", "ё" => "e", "ж" => "j",
+        text = UnicodeUtils.downcase(text)
+
+        ru_mapping = {"а" => "a", "б" => "b", "в" => "v", "г" => "g", "д" => "d", "е" => "e", "ё" => "e", "ж" => "j",
          "з" => "z", "и" => "i", "й" => "i", "к" => "k", "л" => "l", "м" => "m", "н" => "n",
          "о" => "o", "п" => "p", "р" => "r", "с" => "c", "т" => "t", "у" => "y", "ф" => "f",
          "х" => "x", "ц" => "c", "ч" => "ch", "ш" => "sh", "щ" => "sh", "ъ" => "", "ь" => "",
-         "э" => "e", "ю" => "y", "я" => "i"}
+         "э" => "e", "ю" => "y", "я" => "i", "ы" => "u", " " => " "}
         rec_text = ""
-        text.to_s.each_char do |char|
+        text.to_s.downcase.each_char do |char|
           if ru_char = ru_mapping[char].presence
             rec_text += ru_char
           else
