e8df94339f8352b392caf3e0542f8f39291ce57f
Alex Opak <opak.alexandr@gmail.com>
2013-03-27 10:32:48 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Topclub::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'topreserve.com.ua' }
   config.middleware.use ExceptionNotifier,
-                        :email_prefix => "TOP_RESERV #{Rails.env.capitalize} ",
-                        :sender_address => %{"Application Error" <opak.alexandr@gmail.com>},
+                        :email_prefix => "[TOP]",
+                        :sender_address => %{"TOProd" <opak.alexandr@gmail.com>},
                         :exception_recipients => %w{opak.alexandr@gmail.com}
 
   # Log the query plan for queries taking more than this (works
