3f4cccfd304430a050fe0fd44d4e094c9e5fbfcc
Alex Opak <opak.alexandr@gmail.com>
2013-05-24 15:19:36 +0300

    fix

--- app/models/defaults/ability.rb
+++ app/models/defaults/ability.rb
@@ -24,8 +24,6 @@ class Ability
 
   def moderator
     can [:read], User, :id => @user.id
-    can [:manage], User, :city_id => @user.city.try(:id)
-
     # temp fix assets
     can :manage, Asset
     @rules += @user.fetch_rules
