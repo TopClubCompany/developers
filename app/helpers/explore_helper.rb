1db8ddf1baccc62e9d9161972609a886669ee6fa
Opak Alex <opak.alexandr@gmail.com>
2013-06-17 21:39:18 +0300

    fix

--- app/helpers/explore_helper.rb
+++ app/helpers/explore_helper.rb
@@ -4,4 +4,12 @@ module ExploreHelper
     place.images.detect { |image| image.is_main }.try(type.to_sym) if place.images
   end
 
+  def support_phone
+    if current_city
+      City.find(current_city).support_phone
+    else
+      ""
+    end
+  end
+
 end
